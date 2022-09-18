9c203e90b0aa44b3c989ffd4187f9249baab0965
shehandilusanka97 <dilumzsanox9@gmail.com>
2022-09-18 06:02:59 +0530

    commit

--- src/main/java/com/springbootacadamy/springbootAcadamy/entity/Customer.java
+++ src/main/java/com/springbootacadamy/springbootAcadamy/entity/Customer.java
@@ -1,4 +1,5 @@
 package com.springbootacadamy.springbootAcadamy.entity;
+
 import com.vladmihalcea.hibernate.type.json.JsonType;
 import org.hibernate.annotations.Type;
 import org.hibernate.annotations.TypeDef;
@@ -10,24 +11,112 @@ import java.util.ArrayList;
 @Entity
 @Table(name = "customer") //apit ona vidhat table ekk hada gnnaw
 @TypeDefs({
-        @TypeDef(name = "json",typeClass = JsonType.class)
+        @TypeDef(name = "json", typeClass = JsonType.class)
 })
 public class Customer {
     @Id
-    @Column(name ="customer_id",length = 45) //apit ona vidihat hada gnn puluwn table eke clm eka
+    @Column(name = "customer_id", length = 45) //apit ona vidihat hada gnn puluwn table eke clm eka
     @GeneratedValue(strategy = GenerationType.AUTO) //Auto generate kranna
     private int customerId;
 
-    @Column(name = "customer_name",length =100, nullable = false)
+    @Column(name = "customer_name", length = 100, nullable = false)
     private String customerName;
 
-    @Column(name = "customer_address",length = 255)
+    @Column(name = "customer_address", length = 255)
     private String customerAddress;
 
     @Column(name = "customer_salary")
     private double customerSalary;
 
     @Type(type = "json")
-    @Column(name = "contact_numbers",columnDefinition = "json") //multiple values awoth json ekakat danaw
+    @Column(name = "contact_numbers", columnDefinition = "json") //multiple values awoth json ekakat danaw
     private ArrayList contactNumbers;
+
+    @Column(name = "nic")
+    private String nic;
+
+    @Column(name = "active_state", columnDefinition = "TINYINT default 1")
+    private boolean activeState;
+
+    public Customer() {
+    }
+
+    public Customer(int customerId, String customerName, String customerAddress, double customerSalary, ArrayList contactNumbers, String nic, boolean activeState) {
+        this.customerId = customerId;
+        this.customerName = customerName;
+        this.customerAddress = customerAddress;
+        this.customerSalary = customerSalary;
+        this.contactNumbers = contactNumbers;
+        this.nic = nic;
+        this.activeState = activeState;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerAddress() {
+        return customerAddress;
+    }
+
+    public void setCustomerAddress(String customerAddress) {
+        this.customerAddress = customerAddress;
+    }
+
+    public double getCustomerSalary() {
+        return customerSalary;
+    }
+
+    public void setCustomerSalary(double customerSalary) {
+        this.customerSalary = customerSalary;
+    }
+
+    public ArrayList getContactNumbers() {
+        return contactNumbers;
+    }
+
+    public void setContactNumbers(ArrayList contactNumbers) {
+        this.contactNumbers = contactNumbers;
+    }
+
+    public String getNic() {
+        return nic;
+    }
+
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
+
+    public boolean isActiveState() {
+        return activeState;
+    }
+
+    public void setActiveState(boolean activeState) {
+        this.activeState = activeState;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "customerId=" + customerId +
+                ", customerName='" + customerName + '\'' +
+                ", customerAddress='" + customerAddress + '\'' +
+                ", customerSalary=" + customerSalary +
+                ", contactNumbers=" + contactNumbers +
+                ", nic='" + nic + '\'' +
+                ", activeState=" + activeState +
+                '}';
+    }
 }
